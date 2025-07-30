80438bb746785354e5c20aa3dd2f07550984a2ab
Jarco <mail@jarcovanbloois.nl>
2025-07-30 15:36:03 +0200

    add react template

---
+++ mywebsite/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Message from './components/Message';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 >My Website</h1>
+      <Message />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ mywebsite/src/components/Message.js
@@ -0,0 +1,4 @@
+function Message(){
+  return <h2> Welcome to my homepage</h2>;
+}
+export default Message;
